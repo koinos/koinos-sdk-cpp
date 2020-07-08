fd63b3c2f5b3e61a2f534a7744da7ab6cba1218d
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-07-08 13:44:47 -0700

    Build koinos-wasi-sdk as a custom target #6

--- cmake/KoinosWasiSDK.cmake
+++ cmake/KoinosWasiSDK.cmake
@@ -1,6 +1,30 @@
 
 add_custom_target(
-   koinos-clang
+   koinos_wasi_sdk
 
-   COMMAND make -C "${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk/"
+   DEPENDS
+      ${CMAKE_C_COMPILER}
+      ${CMAKE_CXX_COMPILER}
+      ${CMAKE_AR}
+      ${CMAKE_LINKER}
+      ${CMAKE_NM}
+      ${CMAKE_OBJCOPY}
+      ${CMAKE_OBJDUMP}
+      ${CMAKE_RANLIB}
+      ${CMAKE_STRIP}
+)
+
+add_custom_command(
+   OUTPUT
+      ${CMAKE_C_COMPILER}
+      ${CMAKE_CXX_COMPILER}
+      ${CMAKE_AR}
+      ${CMAKE_LINKER}
+      ${CMAKE_NM}
+      ${CMAKE_OBJCOPY}
+      ${CMAKE_OBJDUMP}
+      ${CMAKE_RANLIB}
+      ${CMAKE_STRIP}
+
+   COMMAND make -C ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk
 )
