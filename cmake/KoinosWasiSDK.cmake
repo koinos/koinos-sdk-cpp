761c668f0593262aea91b80a660570b479b9f2ee
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-07-10 15:48:17 -0700

    Progress with installing all necessary files

--- cmake/KoinosWasiSDK.cmake
+++ cmake/KoinosWasiSDK.cmake
@@ -1,6 +1,6 @@
 include( ExternalProject )
 
-set( KOINOS_WASI_SDK_INSTALL_PATH ${CMAKE_BINARY_DIR}/install/ )
+set( KOINOS_WASI_SDK_INSTALL_PATH ${CMAKE_BINARY_DIR}/install )
 set( KOINOS_WASI_SDK_BUILD_PATH ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk/build/install )
 
 if( NOT KOINOS_WASI_SDK_PATH )
@@ -12,12 +12,16 @@ if( NOT KOINOS_WASI_SDK_PATH )
    )
 
    add_custom_command(
-      COMMAND make -C ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk &&
-         mv ${KOINOS_WASI_SDK_BUILD_PATH}/* ${KOINOS_WASI_SDK_INSTALL_PATH} &&
+      COMMAND make -C ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk VERBOSE=1 &&
+         mv ${KOINOS_WASI_SDK_BUILD_PATH}/* ${KOINOS_WASI_SDK_INSTALL_PATH}/ &&
          touch ${CMAKE_BINARY_DIR}/koinos_wasi_sdk.BUILT
 
       OUTPUT ${CMAKE_BINARY_DIR}/koinos_wasi_sdk.BUILT
    )
+
+   install( DIRECTORY ${KOINOS_WASI_SDK_INSTALL_PATH}/wasi-sdk/ DESTINATION wasi-sdk )
+
+   set( KOINOS_WASI_SDK_PATH ${KOINOS_WASI_SDK_INSTALL_PATH}/wasi-sdk )
 
 else()
 
