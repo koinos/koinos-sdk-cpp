9ff56df8b24a98300547e094e73873b725703b7a
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-07-09 17:35:10 -0700

    Ship it!

--- cmake/KoinosWasiSDK.cmake
+++ cmake/KoinosWasiSDK.cmake
@@ -1,19 +1,22 @@
 include( ExternalProject )
 
+set( KOINOS_WASI_SDK_INSTALL_PATH ${CMAKE_BINARY_DIR}/install/ )
+set( KOINOS_WASI_SDK_BUILD_PATH ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk/build/install )
+
 if( NOT KOINOS_WASI_SDK_PATH )
 
    add_custom_target(
       koinos_wasi_sdk
 
-      COMMAND mv -f ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk/build/install/* ${CMAKE_BINARY_DIR}/install/
-
-      DEPENDS ${CMAKE_BUILD_DIRECTORY}/koinos_wasi_sdk.BUILT
+      DEPENDS ${CMAKE_BINARY_DIR}/koinos_wasi_sdk.BUILT
    )
 
    add_custom_command(
-      COMMAND make -C ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk
+      COMMAND make -C ${CMAKE_SOURCE_DIR}/host/koinos-wasi-sdk &&
+         mv ${KOINOS_WASI_SDK_BUILD_PATH}/* ${KOINOS_WASI_SDK_INSTALL_PATH} &&
+         touch ${CMAKE_BINARY_DIR}/koinos_wasi_sdk.BUILT
 
-      OUTPUT ${CMAKE_BUILD_DIRECTORY}/koinos_wasi_sdk.BUILT
+      OUTPUT ${CMAKE_BINARY_DIR}/koinos_wasi_sdk.BUILT
    )
 
 else()
