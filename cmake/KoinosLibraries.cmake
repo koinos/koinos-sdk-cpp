16c5dadc3b78af653576ac06436fb12f134924d4
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-07-13 14:24:50 -0700

    Fix libraries install

--- cmake/KoinosLibraries.cmake
+++ cmake/KoinosLibraries.cmake
@@ -3,14 +3,13 @@ include(ExternalProject)
 find_package(Boost 1.57 REQUIRED)
 
 ExternalProject_Add(
-  KoinosWasmLibraries
-  SOURCE_DIR "${CMAKE_SOURCE_DIR}/libraries"
-  BINARY_DIR "${CMAKE_BINARY_DIR}/libraries"
-  CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_BINARY_DIR}/lib/cmake/koinos-cdt/KoinosWasmToolchain.cmake -DBoost_INCLUDE_DIR=${Boost_INCLUDE_DIR} -DBUILD_TYPES_TESTS=OFF
-  UPDATE_COMMAND ""
-  PATCH_COMMAND  ""
-  TEST_COMMAND   ""
-  INSTALL_COMMAND ""
-  BUILD_ALWAYS 1
-  DEPENDS koinos_wasi_sdk
+   KoinosWasmLibraries
+   SOURCE_DIR "${CMAKE_SOURCE_DIR}/libraries"
+   BINARY_DIR "${CMAKE_BINARY_DIR}/libraries"
+   CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_BINARY_DIR}/lib/cmake/koinos-cdt/KoinosWasmToolchain.cmake -DBoost_INCLUDE_DIR=${Boost_INCLUDE_DIR} -DBUILD_TYPES_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+   UPDATE_COMMAND ""
+   PATCH_COMMAND  ""
+   TEST_COMMAND   ""
+   BUILD_ALWAYS 1
+   DEPENDS koinos_wasi_sdk
 )
