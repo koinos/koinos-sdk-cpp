2fc92444d4a466fd781ed2a35d6f56629cd57f83
Ron <brklyn8900@gmail.com>
2020-07-24 14:26:57 -0400

    #22: Document set up process

---
+++ CDT-README.md
@@ -0,0 +1,6 @@
+### Installation:
+
+1. execute command `./koinos_install.sh`
+2. Supply the desired installation directory (note: directory will be relative to current path unless explicitly provided absolute path)
+
+The CDT is now installed in the selected directory Enjoy!
--- koinos-install.sh
+++ koinos-install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+echo "Unless explicitly specified provided path will be relative to current directory"
 read -p "Enter desired installation directory name: " install_dir
 
 mkdir $install_dir
 
 tar xfv koinos-cdt.tar.gz -C $install_dir
 
-echo "\n"
-echo "Great success! Koinos CDT installed in the $install_dir directory"
+echo -e "\nGreat success! Koinos CDT installed in the $install_dir directory\n"
