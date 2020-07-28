17a456f409441b4d2adebb31427e358c7b930062
Ron <brklyn8900@gmail.com>
2020-07-28 12:23:59 -0400

    #23: fix tar error

--- make_pkg.sh
+++ make_pkg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf host/koinos-wasi-sdk/build
+
 mkdir build/
 
 cd build
@@ -8,11 +10,11 @@ mkdir koinos-cdt
 
 cmake -DCMAKE_INSTALL_PREFIX=`pwd`/koinos-cdt ..
 
-make 
+make
 
 make install
 
-tar -czvf koinos-cdt.tar.gz build/koinos-cdt
+tar -czvf koinos-cdt.tar.gz koinos-cdt
 
 
 
