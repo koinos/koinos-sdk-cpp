6eefa6a0a03d33db6073904f82395547c977b20f
Ron <brklyn8900@gmail.com>
2020-07-29 20:27:00 -0400

    #22 remove install and make scripts

--- koinos-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Unless explicitly specified provided path will be relative to current directory"
-read -p "Enter desired installation directory name: " install_dir
-
-mkdir $install_dir
-
-tar xfv koinos-cdt.tar.gz -C $install_dir
-
-echo -e "\nGreat success! Koinos CDT installed in the $install_dir directory\n"
