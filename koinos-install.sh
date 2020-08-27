993c95cad77252ebe3371d313c3aec36294d13e8
Steve Gerbino <steve@gerbino.co>
2020-08-27 15:54:59 -0400

    #24: Make bash script take command line arguments

--- koinos-install.sh
+++ koinos-install.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
-set -e
-set -x
 
-read -p "Enter desired installation directory name: " install_dir
+usage="$(basename "$0") [-h] [-d dir] -- program to install the koinos contract development toolkit
 
-mkdir $install_dir
+where:
+    -h  show this help text
+    -d  the installation directory"
 
-tar xfv koinos-cdt.tar.gz -C $install_dir
+while getopts ':d:h' flag
+do
+   case "${flag}" in
+      h) echo "$usage"
+         exit
+         ;;
+      d) install_dir=${OPTARG};;
+      :) printf "invalid option: -%s requires an argument\n" "$OPTARG" >&2
+         echo "$usage" >&2
+         exit 1
+         ;;
+      \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+         echo "$usage" >&2
+         exit 1
+         ;;
+   esac
+done
 
-echo "\n"
-echo "Great success! Koinos CDT installed in the $install_dir directory"
+mkdir -p $install_dir
+
+tar xfv build/koinos-cdt.tar.gz -C $install_dir
+
+echo "Koinos CDT installed to $install_dir"
+
