84fa75bb03ada3ed14f399526b157d4bcbf1125a
Steve Gerbino <steve@gerbino.co>
2021-05-04 03:09:18 -0400

    #38: Extract tar in to /opt directory

--- ci/install.sh
+++ ci/install.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-sudo curl -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-12.0-linux.tar.gz -o wasi-sdk-12.tar.gz
-sudo tar -xvzf wasi-sdk-12.tar.gz -C /
+sudo curl -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-12.0-linux.tar.gz -o wasi-sdk-12.0.tar.gz
+sudo tar -xvzf wasi-sdk-12.0.tar.gz -C /opt
 
