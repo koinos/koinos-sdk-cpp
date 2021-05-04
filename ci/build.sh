a9c7c13025dcfbe382ea95542df3cedcc80f8fa9
Steve Gerbino <steve@gerbino.co>
2021-05-04 03:40:07 -0400

    #38: Install prefix within the user home directory

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,6 @@ set -x
 export KOINOS_WASI_SDK_ROOT=/opt/wasi-sdk-12.0
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/opt ..
 cmake --build . --config Release --parallel 3
 
