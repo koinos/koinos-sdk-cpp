3ecb31e9689079a5155105184b02095858fe0c88
Steve Gerbino <steve@gerbino.co>
2021-05-04 18:26:03 -0400

    #38: Install the target as part of the build process

--- ci/build.sh
+++ ci/build.sh
@@ -7,5 +7,5 @@ export KOINOS_WASI_SDK_ROOT=$HOME/opt/wasi-sdk-12.0
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/opt/koinos-cdt ..
-cmake --build . --config Release --parallel 3
+cmake --build . --config Release --target install --parallel 3
 
