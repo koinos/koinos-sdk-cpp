ae950072a915feebfc1ff684707da589d1603a93
Steve Gerbino <steve@gerbino.co>
2021-05-04 18:23:53 -0400

    #38: Ensure that all necessary files are installed to the CMAKE_INSTALL_PREFIX as well as simplify CMake super project

--- cmake/KoinosConfigureToolchain.cmake
+++ cmake/KoinosConfigureToolchain.cmake
@@ -1,8 +1,3 @@
-set(CDT_ROOT_DIR "${CMAKE_BINARY_DIR}/install")
+configure_file(${CMAKE_SOURCE_DIR}/cmake/KoinosWasmToolchain.cmake.in ${CMAKE_BINARY_DIR}/cmake/koinos-wasm-toolchain.cmake @ONLY)
 
-configure_file(${CMAKE_SOURCE_DIR}/cmake/KoinosWasmToolchain.cmake.in ${CMAKE_BINARY_DIR}/lib/cmake/koinos-cdt/KoinosWasmToolchain.cmake @ONLY)
-
-set(CDT_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
-
-configure_file(${CMAKE_SOURCE_DIR}/cmake/KoinosWasmToolchain.cmake.in ${CMAKE_BINARY_DIR}/cmake/KoinosWasmToolchain.cmake @ONLY)
-
+install(FILES ${CMAKE_BINARY_DIR}/cmake/koinos-wasm-toolchain.cmake DESTINATION cmake)
