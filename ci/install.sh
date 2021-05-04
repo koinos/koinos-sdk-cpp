57fe7dd9d9035b541c5883161692692353072c34
Steve Gerbino <steve@gerbino.co>
2021-05-04 04:02:00 -0400

    #38: Create directory if it doesn't exist

--- ci/install.sh
+++ ci/install.sh
@@ -4,6 +4,8 @@ set -e
 set -x
 
 curl -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-12.0-linux.tar.gz -o wasi-sdk-12.0.tar.gz
+
+mkdir -p $HOME/opt
 tar -xvzf wasi-sdk-12.0.tar.gz -C $HOME/opt
 
 pip3 install --user dataclasses_json Jinja2 importlib_resources pluginbase gitpython
