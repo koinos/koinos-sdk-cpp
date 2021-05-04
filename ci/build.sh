c008975a5012820b723f5e5e5174ae05718d4427
Steve Gerbino <steve@gerbino.co>
2021-05-04 03:55:42 -0400

    #38: Remove the need for sudo, build everything at home

--- ci/build.sh
+++ ci/build.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
-export KOINOS_WASI_SDK_ROOT=/opt/wasi-sdk-12.0
+export KOINOS_WASI_SDK_ROOT=$HOME/opt/wasi-sdk-12.0
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/opt ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/opt/koinos-cdt ..
 cmake --build . --config Release --parallel 3
 
--- ci/install.sh
+++ ci/install.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-sudo curl -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-12.0-linux.tar.gz -o wasi-sdk-12.0.tar.gz
-sudo tar -xvzf wasi-sdk-12.0.tar.gz -C /opt
+curl -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-12.0-linux.tar.gz -o wasi-sdk-12.0.tar.gz
+tar -xvzf wasi-sdk-12.0.tar.gz -C $HOME/opt
 
 pip3 install --user dataclasses_json Jinja2 importlib_resources pluginbase gitpython
 
