c0a8f69145e2808f2804bcd65fc74d6e7d11c7fe
Steve Gerbino <steve@gerbino.co>
2021-09-16 15:04:14 -0400

    #51: Use install PROGRAMS so that scripts are executable

--- cmake/KoinosProtoInstall.cmake
+++ cmake/KoinosProtoInstall.cmake
@@ -1,3 +1,4 @@
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/scripts/ DESTINATION scripts)
+install(PROGRAMS ${PROJECT_SOURCE_DIR}/scripts/koinos_generate_proto.sh DESTINATION scripts)
+install(PROGRAMS ${PROJECT_SOURCE_DIR}/scripts/koinos_generate_proto.bat DESTINATION scripts)
 install(FILES ${PROJECT_SOURCE_DIR}/cmake/KoinosProto.cmake DESTINATION cmake)
 
