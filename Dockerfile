da12915020ae4eefac4230b363a1e31110562756
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-10-28 17:30:20 -0700

    Progress on docker CDT build

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,19 @@ FROM koinos/koinos-cdt-toolchain:latest as builder
 ADD . /koinos-cdt
 WORKDIR /koinos-cdt
 
-RUN mkdir build && \
+RUN cd $KOINOS_WASI_SDK_ROOT/bin && ls -la && cd /koinos-cdt && \
+    mkdir build && \
     cd build && \
-    cmake -DCMAKE_INSTALL_PREFIX=~/opt/koinos-cdt .. && \
+    cmake -DCMAKE_INSTALL_PREFIX=/opt/koinos-cdt .. && \
     make -j install
 
 FROM koinos/koinos-cdt-toolchain:latest
 COPY --from=builder /opt/koinos-cdt /opt/koinos-cdt
 
-ENV KOINOS_CDT_ROOT=~/opt/koinos-cdt
+RUN mkdir -p /sources
+RUN apt install python3.8-venv
+
+ENV KOINOS_CDT_ROOT=/opt/koinos-cdt
 ADD ./docker/ /opt
+
+ENTRYPOINT [ "/opt/scripts/build.sh" ]
--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -5,26 +5,21 @@ set -x
 
 # copy sources in to project
 cd /sources
-cp *.proto /opt/project/types
-cp *.c     /opt/project/src
-cp *.cpp   /opt/project/src
-cp *.h     /opt/project/src
-cp *.hpp   /opt/project/src
+find . -name \*.proto -exec cp {} /opt/project/types \;
+find . -name \*.c     -exec cp {} /opt/project/src \;
+find . -name \*.cpp   -exec cp {} /opt/project/src \;
+find . -name \*.h     -exec cp {} /opt/project/src \;
+find . -name \*.hpp   -exec cp {} /opt/project/src \;
 
 # build contract
-cp -R /opt/project /tmp/project
-cp /tmp/project
+cd /opt/project
 mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=${KOINOS_CDT_ROOT}/cmake/koinos-wasm-toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j
+make contract
 
 # copy build artifacts
 cd src
 cp contract.wasm /sources
 cd ../types
 cp types.pb /sources
-
-# cleanup project source
-cd ~
-rm -rf /tmp/project
