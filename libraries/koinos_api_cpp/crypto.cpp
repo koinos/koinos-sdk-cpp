baac092a942ce5bf7315f919cc518b6d358d2669
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-02-16 15:18:09 -0800

    Don't use stringstream

--- libraries/koinos_api_cpp/crypto.cpp
+++ libraries/koinos_api_cpp/crypto.cpp
@@ -1,6 +1,6 @@
 #include <koinos/crypto.hpp>
 
-#include <sstream>
+#include <koinos/buffer.hpp>
 
 namespace koinos {
 
@@ -11,41 +11,38 @@ struct unsigned_varint
    uint64_t value = 0;
 };
 
-void to_binary( std::ostream& s, const unsigned_varint& v )
+void to_binary( EmbeddedProto::WriteBufferInterface& s, const unsigned_varint& v )
 {
-   char tmp[ ( sizeof( v.value ) * 8 + 6 ) / 7 ];
+   uint8_t tmp[ ( sizeof( v.value ) * 8 + 6 ) / 7 ];
    uint64_t n = v.value;
    uint32_t len = 0;
 
    while ( true )
    {
-      tmp[len] = ( n & 0x7F );
+      tmp[ len ] = ( n & 0x7F );
 
       if ( n <= 0x7F )
          break;
 
-      tmp[len] |= 0x80;
+      tmp[ len ] |= 0x80;
       n = ( n >> 7 );
       len++;
    }
 
-   for ( uint32_t i = 0; i <= len; i++ )
-   {
-      s.write( tmp + i, sizeof( char ) );
-   }
+   s.push( tmp, len + 1 );
 }
 
-void from_binary( std::istream& s, unsigned_varint& v )
+void from_binary( EmbeddedProto::ReadBufferInterface& s, unsigned_varint& v )
 {
    v.value = 0;
-   char data = 0;
+   uint8_t data = 0;
    uint32_t i = 0;
    do
    {
-      s.get( data );
-
-      if ( !( s.good() ) )
+      if ( !s.get_size() )
          throw std::runtime_error( "error reading from stream" );
+
+      s.pop( data );
 
       v.value |= ( data & 0x7f ) << i;
       i += 7;
@@ -54,31 +51,36 @@ void from_binary( std::istream& s, unsigned_varint& v )
 
 void multihash::deserialize( const std::string& s )
 {
-   std::stringstream ss( s );
+   read_buffer rb( reinterpret_cast< const uint8_t* >( s.data() ), s.size() );
 
    unsigned_varint mh_code;
    unsigned_varint mh_size;
 
-   from_binary( ss, mh_code );
-   from_binary( ss, mh_size );
+   from_binary( rb, mh_code );
+   from_binary( rb, mh_size );
 
    digest.resize( mh_size.value );
-   ss.read( reinterpret_cast< char* >( digest.data() ), mh_size.value );
+   auto data = reinterpret_cast< uint8_t* >( digest.data() );
+   for ( uint64_t i = 0; i < mh_size.value; i++ )
+   {
+      rb.pop( data[ i ] );
+   }
 
    code = mh_code.value;
 }
 
 std::string multihash::serialize()
 {
+   uint8_t buffer[ 532 ];
+   write_buffer wb( buffer, 532 );
+
    unsigned_varint mh_code{ code };
    unsigned_varint mh_size{ digest.size() };
 
-   std::stringstream ss;
+   to_binary( wb, mh_code );
+   to_binary( wb, mh_size );
 
-   to_binary( ss, mh_code );
-   to_binary( ss, mh_size );
-
-   ss.write( digest.data(), digest.size() );
+   wb.push( reinterpret_cast< const uint8_t* >( digest.data() ), digest.size() );
 }
 
 } // koinos
